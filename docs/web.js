e8bc7f41c17a53e6ed2437e10e4b671d38009dac
matriz <cyruschan0111@gmail.com>
2020-04-11 11:58:49 +0800

    web

---
+++ docs/web.js
@@ -0,0 +1,9 @@
+function gotopyloading(){
+    window.location.href = "https://github.com/windowsboy111/pyloading/";
+}
+function gotokccsofficial(){
+    window.location.href = 'https://sites.google.com/view/kccsofficial/home/';
+}
+function gotogitkccs(){
+    window.location.href = 'https://github.com/windowsboy111/kccs-official/';
+}
