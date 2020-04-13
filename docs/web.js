0eef6b9f4e90d3d5749ac13598027fdadbbc5248
matriz <cyruschan0111@gmail.com>
2020-04-13 09:42:08 +0800

    web

--- docs/web.js
+++ docs/web.js
@@ -1,14 +1,4 @@
-function gotopyloading(){
-    window.location.href = "https://github.com/windowsboy111/pyloading/";
-}
-function gotokccsofficial(){
-    window.location.href = 'https://sites.google.com/view/kccsofficial/home?authuser=0';
-}
-function gotogitkccs(){
-    window.location.href = 'https://github.com/windowsboy111/kccs-official/';
-}
-
 document.getElementById('twitter').setAttribute('src','https://i.ya-webdesign.com/images/instagram-clipart-translucent-4.png');
-iglogo = document.getElementById('instagram')
+iglogo = document.getElementById('instagram');
 iglogo.setAttribute('src','https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS2SYUy45LDdMaosc-gGyFdfAVclLpwgb29nzyTJvLGVjhB_ojE&usqp=CAU');
-iglogo.setAttribute('background-color','#111111')
+iglogo.setAttribute('background-color','#111111');
